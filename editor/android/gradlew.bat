4f3c0a014a7b9adfd493a3aedafcde951d755c65
homy <leo740143@gmail.com>
2026-06-10 03:25:06 +0300

    Initial commit from ZIP upload

---
+++ editor/android/gradlew.bat
@@ -0,0 +1,13 @@
+@rem Gradle startup script for Windows
+@if "%DEBUG%"=="" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set JAVA_EXE=java.exe
+"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+:end
+if "%OS%"=="Windows_NT" endlocal
